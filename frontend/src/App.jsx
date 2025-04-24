11c9aa21830c05e03eb077ec85274fd74bb2c7d6
jpedro058 <joao_pedro_12345@hotmail.com>
2025-04-24 15:30:28 +0100

    Add main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,68 @@
 import "./App.css";
+import brokenScreen from "./assets/brokenScreen.png";
+import bateria from "./assets/bateria.png";
+import software from "./assets/software.png";
+import virus from "./assets/virus.png";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+
 function App() {
+  const services = [
+    {
+      title: "Substituição de Ecrã",
+      desc: "O ecrã do seu dispositivo está partido, riscado ou sem resposta ao toque? A nossa equipa especializada realiza a substituição com peças de alta qualidade, garantindo a integridade e a sensibilidade original do ecrã. Serviço rápido e com garantia.",
+      img: brokenScreen,
+    },
+    {
+      title: "Troca de Bateria",
+      desc: "Nota que o seu dispositivo descarrega rapidamente ou desliga-se inesperadamente? Efetuamos a substituição da bateria por uma nova, com desempenho garantido e compatibilidade certificada, para que possa voltar a confiar na autonomia do seu equipamento.",
+      img: bateria,
+    },
+    {
+      title: "Problemas de Software",
+      desc: "O seu computador está lento, apresenta erros constantes ou não arranca corretamente? Fazemos uma análise completa do sistema operativo e software instalado, solucionamos conflitos, otimizamos o desempenho e garantimos uma utilização fluida e segura.",
+      img: software,
+    },
+    {
+      title: "Remoção de Vírus",
+      desc: "Detetou comportamentos estranhos, pop-ups ou ficheiros suspeitos no seu sistema? Utilizamos ferramentas avançadas para remover vírus, malware e spyware, restaurando a segurança e a estabilidade do seu dispositivo sem comprometer os seus dados.",
+      img: virus,
+    },
+  ];
+
   return (
-    <div className="bg-red-500">
-      <h1>a</h1>
+    <div className="bg-[#0F3D57] min-h-screen text-white font-sans">
+      <Navbar />
+
+      <section className="px-12 py-16 min-h-[calc(100vh-4rem)]">
+        <h2 className="text-center text-5xl font-bold mb-14 text-[#00B8D9]">
+          Os nossos serviços
+        </h2>
+
+        <div className="grid gap-10 grid-cols-1 md:grid-cols-2">
+          {services.map((s) => (
+            <div
+              key={s.title}
+              className="bg-[#145374] cursor-pointer rounded-3xl p-8 shadow-xl hover:shadow-2xl transition duration-300 flex flex-col md:flex-row items-center gap-6 hover:scale-[1.02]"
+            >
+              <img
+                src={s.img}
+                alt={s.title}
+                className="h-65 rounded-xl 
+              aspect-[1/1] object-cover object-center"
+              />
+              <div className="flex flex-col gap-8">
+                <h3 className="text-3xl font-semibold text-[#00B8D9]">
+                  {s.title}
+                </h3>
+                <p className="text-xl text-slate-100 leading-8">{s.desc}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      <Footer />
     </div>
   );
 }
